6efc212c448401dd094f3896823434efcd0bacee
dosbrunos <94176523+dosbrunos@users.noreply.github.com>
2024-02-09 10:33:54 +0100

    Update Part-II.py

--- Part-II.py
+++ Part-II.py
@@ -1,66 +1,68 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-def buildTree( S , vol , T, N ):
+def buildTree(S, vol, T, N):
     dt = T / N
-    matrix = np.zeros( (N + 1 , N + 1 ) )
-    u = 0 # TODO
-    d = 0 # TODO
+    matrix = np.zeros((N + 1 , N + 1))
+    matrix[0,0] = S
+    u = np.exp(sigma*np.sqrt(dt))
+    d = np.exp(-sigma*np.sqrt(dt))
     # Iterate over the lower triangle
 
     for i in np.arange(N + 1 ): # iiterate over rows
         for j in np.arange( i + 1 ): # iterate overs columns
-# Hint : express each cell as a combination of up
-# and down moves
-            matrix[ i , j ] = 0 # TODO
+    # Hint : express each cell as a combination of up
+    # and down moves
+            matrix[i ,j] = S*u**(j)*d**(i-j)
             return matrix
         
 sigma = 0.1
 S = 80
 T = 1. 
 N = 2
-buildTree(S , sigma , T, N)       
+Tree = buildTree(S, sigma , T, N)     
+print(Tree)  
 
-def valueOptionMatrix(tree , T, r , K, vol ):
-    dt = T / N
-    u = 0 # TODO
-    d = 0 # TODO
-    p = 0 # TODO
-    columns = tree.shape[ 1 ]
-    rows = tree.shape[ 0 ]
-    # Walk backward , we start in last row of the matrix
-    # Add the payoff function in the last row
-    for c in np.arange( columns ):
-        S = tree[rows - 1 , c ] # value in the matrix
-        tree[rows - 1 , c ] = 0 # TODO
+# def valueOptionMatrix(tree , T, r , K, vol ):
+#     dt = T / N
+#     u = 0 # TODO
+#     d = 0 # TODO
+#     p = 0 # TODO
+#     columns = tree.shape[ 1 ]
+#     rows = tree.shape[ 0 ]
+#     # Walk backward , we start in last row of the matrix
+#     # Add the payoff function in the last row
+#     for c in np.arange( columns ):
+#         S = tree[rows - 1 , c ] # value in the matrix
+#         tree[rows - 1 , c ] = 0 # TODO
 
-    # For all other rows , we need to combine from previous rows
-    # We walk backwards , from the last row to the first row
-    for i in np.arange(rows - 1 )[ : : - 1 ]:
-        for j in np.arange( i + 1 ):
-        down = tree[i + 1 , j ]
-        up = tree[i + 1 , j + 1 ]
-        tree[i , j ] = 0 # TODO
-    return tree
+#     # For all other rows , we need to combine from previous rows
+#     # We walk backwards , from the last row to the first row
+#     for i in np.arange(rows - 1 )[ : : - 1 ]:
+#         for j in np.arange( i + 1 ):
+#         down = tree[i + 1 , j ]
+#         up = tree[i + 1 , j + 1 ]
+#         tree[i , j ] = 0 # TODO
+#     return tree
 
-sigma = 0.1
-S = 80
-T = 1.
-N = 2
-K = 85
-r = 0.1
-tree = buildTree(S , sigma , T, N)
-valueOptionMatrix(tree , T, r , K, sigma)
+# sigma = 0.1
+# S = 80
+# T = 1.
+# N = 2
+# K = 85
+# r = 0.1
+# tree = buildTree(S , sigma , T, N)
+# valueOptionMatrix(tree , T, r , K, sigma)
 
-# Play around with different ranges of N and stepsizes.
+# # Play around with different ranges of N and stepsizes.
 
-N = np.arange(1 , 300)
-# calculate the option price for the coorect parameters
-optionPriceAnalytical = 0 # TODO 
-# calculate the option price for each n in N
-for n in N:
-    treeN = buildTree() # TODO
-    priceApproximatedly = valueOption() # TODO
+# N = np.arange(1 , 300)
+# # calculate the option price for the coorect parameters
+# optionPriceAnalytical = 0 # TODO 
+# # calculate the option price for each n in N
+# for n in N:
+#     treeN = buildTree() # TODO
+#     priceApproximatedly = valueOption() # TODO
 
-# use matplotlib to plot the analytical value
-# and the approximated value for each n
+# # use matplotlib to plot the analytical value
+# # and the approximated value for each n
