dab86feae86989be1dd1c820b3ed47247e9023c0
dosbrunos <94176523+dosbrunos@users.noreply.github.com>
2024-02-14 09:18:34 +0100

    Update Part-II.py

--- Part-II.py
+++ Part-II.py
@@ -10,50 +10,51 @@ def buildTree(S, vol, T, N):
     # Iterate over the lower triangle
 
     for i in np.arange(N + 1 ): # iiterate over rows
-        for j in np.arange( i + 1 ): # iterate overs columns
-    # Hint : express each cell as a combination of up
-    # and down moves
-            matrix[i ,j] = S*u**(j)*d**(i-j)
-            return matrix
+        for j in np.arange(i + 1 ): # iterate overs columns
+            # Hint : express each cell as a combination of up
+            # and down moves
+            matrix[i, j] = S*u**(j)*d**(i-j)
+    return matrix
         
-sigma = 0.1
-S = 80
-T = 1. 
-N = 2
-Tree = buildTree(S, sigma , T, N)     
-print(Tree)  
-
-# def valueOptionMatrix(tree , T, r , K, vol ):
-#     dt = T / N
-#     u = 0 # TODO
-#     d = 0 # TODO
-#     p = 0 # TODO
-#     columns = tree.shape[ 1 ]
-#     rows = tree.shape[ 0 ]
-#     # Walk backward , we start in last row of the matrix
-#     # Add the payoff function in the last row
-#     for c in np.arange( columns ):
-#         S = tree[rows - 1 , c ] # value in the matrix
-#         tree[rows - 1 , c ] = 0 # TODO
-
-#     # For all other rows , we need to combine from previous rows
-#     # We walk backwards , from the last row to the first row
-#     for i in np.arange(rows - 1 )[ : : - 1 ]:
-#         for j in np.arange( i + 1 ):
-#         down = tree[i + 1 , j ]
-#         up = tree[i + 1 , j + 1 ]
-#         tree[i , j ] = 0 # TODO
-#     return tree
-
 # sigma = 0.1
 # S = 80
-# T = 1.
+# T = 1. 
 # N = 2
-# K = 85
-# r = 0.1
-# tree = buildTree(S , sigma , T, N)
-# valueOptionMatrix(tree , T, r , K, sigma)
+# Tree = buildTree(S, sigma , T, 50)     
 
+
+def valueOptionMatrix(tree, T, r, K, vol ):
+    dt = T / N
+    u = np.exp(sigma*np.sqrt(dt))
+    d = np.exp(-sigma*np.sqrt(dt))
+    p = (np.exp(r*dt) - d)/ (u - d)
+    columns = tree.shape[1]
+    rows = tree.shape[0]
+    # Walk backward , we start in last row of the matrix
+    # Add the payoff function in the last row
+    for c in np.arange( columns ):
+        S = tree[rows - 1, c ] # value in the matrix
+        # Payoff function
+        tree[rows - 1, c ] = max(S - K, 0) # TODO
+
+    # For all other rows , we need to combine from previous rows
+    # We walk backwards , from the last row to the first row
+    for i in np.arange(rows - 1 )[ : : - 1 ]:
+        for j in np.arange( i + 1 ):
+            down = tree[i + 1, j ]
+            up = tree[i + 1, j + 1 ]
+            tree[i, j ] = np.exp(-r*dt)*(p*up-(1-p)*down) # TODO
+    return tree
+
+sigma = 0.1
+S = 80
+T = 1.
+N = 50
+K = 85
+r = 0.1
+tree = buildTree(S , sigma , T, N)
+optionPrice = valueOptionMatrix(tree, T, r, K, sigma)
+print(optionPrice)
 # # Play around with different ranges of N and stepsizes.
 
 # N = np.arange(1 , 300)
