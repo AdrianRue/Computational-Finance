dfc20f29a6f011203ae4b1cc384e59dd97da0d1e
AdrianRue <aruesink13@gmail.com>
2024-02-28 15:49:31 +0100

    added convergence

--- Assignment-2/Part-I.py
+++ Assignment-2/Part-I.py
@@ -18,7 +18,7 @@ def average_payoff(S0, K, T, r, sigma, dt, num_simulations, num_steps):
 
     for i in range(num_simulations):
         stock_prices = stock_sim(S0, T, r, sigma, dt, num_steps)
-        payoff = np.maximum(stock_prices[-1] - K, 0)
+        payoff = np.maximum(K - stock_prices[-1], 0)
         payoffs[i] = payoff
 
     return np.mean(payoffs)
@@ -34,7 +34,46 @@ def run_simulation():
     num_steps = int(T / dt)
 
     payoff = average_payoff(S0, K, T, r, sigma, dt, num_simulations, num_steps)
-    option_price = np.exp(-r * T) * payoff/num_simulations
+    option_price = np.exp(-r * T) * payoff
     print(option_price)
 
 print(run_simulation())
+
+def converge_payoff(S0, K, T, r, sigma, dt, num_simulations, num_steps):
+
+    payoffs = []
+
+    mean_payoffs = []
+    i_s = []
+    for i in range(num_simulations):
+
+        stock_prices = stock_sim(S0, T, r, sigma, dt, num_steps)
+        payoff = np.maximum(K - stock_prices[-1], 0)
+        payoffs.append(payoff)
+
+        if i % 10 == 0:
+            mean_payoffs.append(np.mean(payoffs))
+            i_s.append(i)
+
+
+    return mean_payoffs, i_s
+
+def convergence():
+
+    S0 = 100
+    K = 99
+    T = 1
+    r = 0.06
+    sigma = 0.2
+    dt = 1 / 365
+    num_simulations = 5000
+    num_steps = int(T / dt)
+
+    payoffs = converge_payoff(S0, K, T, r, sigma, dt, num_simulations, num_steps)
+    plt.plot(payoffs[1], payoffs[0])
+    plt.show()
+
+convergence()
+
+
+
